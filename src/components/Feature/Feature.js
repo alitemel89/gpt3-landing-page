739c40cc9cb55ea9abe7f137e7986d239c7434c4
alitemel89 <alitemel89.ce@gmail.com>
2021-12-11 19:28:38 +0300

    update Footer.css

--- src/components/Feature/Feature.js
+++ src/components/Feature/Feature.js
@@ -9,7 +9,7 @@ const Feature = ({ title, text }) => {
         <h1>{title}</h1>
       </div>
       <div className="gpt3__features-container_feature-text">
-          <p>{text}</p>
+        <p>{text}</p>
       </div>
     </div>
   );
